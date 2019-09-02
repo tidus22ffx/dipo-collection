fb80f430a3238a35f4fc236e55f30a108a990877
zharfananantha <zharfan.dark@gmail.com>
2019-09-02 10:32:37 +0700

    setting activity

--- app/src/main/java/com/example/mobilecollection/View/SettingsActivity.java
+++ app/src/main/java/com/example/mobilecollection/View/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.mobilecollection.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.example.mobilecollection.R;
@@ -12,5 +14,12 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_setting);
+
+        toolbar.setTitle("Setting");
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24dp);
+        setSupportActionBar(toolbar);
     }
 }
