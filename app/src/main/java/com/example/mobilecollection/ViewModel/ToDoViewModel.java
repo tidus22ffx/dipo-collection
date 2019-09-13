2bf118a067cecbb0579b13e685e613cdb3d2d60e
zharfananantha <zharfan.dark@gmail.com>
2019-09-13 16:54:36 +0700

    to do submit to pending

--- app/src/main/java/com/example/mobilecollection/ViewModel/ToDoViewModel.java
+++ app/src/main/java/com/example/mobilecollection/ViewModel/ToDoViewModel.java
@@ -1,29 +1,37 @@
 package com.example.mobilecollection.ViewModel;
 
+import android.app.Application;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.mobilecollection.Repository.API.ApiService;
+import com.example.mobilecollection.Repository.DB.AppDatabase;
 import com.example.mobilecollection.Repository.Model.TodoItem;
 
 import java.util.ArrayList;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableCompletableObserver;
 import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
 
-public class ToDoViewModel extends ViewModel {
+public class ToDoViewModel extends AndroidViewModel {
     ApiService service = new ApiService();
 
     MutableLiveData<ArrayList<TodoItem>> todoList = new MutableLiveData<>();
-    MutableLiveData<TodoItem> todoItemDetail = new MutableLiveData<>();
     MutableLiveData<Boolean> loading = new MutableLiveData<>();
     MutableLiveData<Boolean> isError = new MutableLiveData<>();
     MutableLiveData<String> errorMessage = new MutableLiveData<>();
     ArrayList<TodoItem> todoItems;
+
+    public ToDoViewModel(@NonNull Application application) {
+        super(application);
+    }
 
     public MutableLiveData<ArrayList<TodoItem>> getTodoList() {
         todoList.setValue(todoItems);
@@ -32,7 +40,6 @@ public class ToDoViewModel extends ViewModel {
 
     public MutableLiveData<ArrayList<TodoItem>> filterList(String param) {
         ArrayList<TodoItem> todoItemsFiltered = new ArrayList<>();
-
         Log.d("ITEMS", "TODOITEMS: "+todoItems);
         if (param.isEmpty() || param.equalsIgnoreCase("")) {
             todoList.setValue(todoItems);
@@ -94,33 +101,6 @@ public class ToDoViewModel extends ViewModel {
                             }
                         })
         );
-    }
-
-    public void fetchDetail(int id) {
-        loading.setValue(true);
-        disposable.add(
-                service.getToDoDetails(id)
-                        .subscribeOn(Schedulers.newThread())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(new DisposableSingleObserver<TodoItem>() {
-                            @Override
-                            public void onSuccess(TodoItem value) {
-                                todoItemDetail.setValue(value);
-                                loading.setValue(false);
-                            }
-
-                            @Override
-                            public void onError(Throwable e) {
-                                loading.setValue(false);
-                                isError.setValue(true);
-                                errorMessage.setValue(e.toString());
-                            }
-                        })
-        );
-    }
-
-    public MutableLiveData<TodoItem> getToDoDetail() {
-        return todoItemDetail;
     }
 
     @Override
