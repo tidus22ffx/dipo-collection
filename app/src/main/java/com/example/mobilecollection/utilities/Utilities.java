9ca653c33b16287a19b76354a575b73c13687294
tidus22ffx <muhammad.faisal@indocyber.co.id>
2019-09-26 11:22:35 +0700

    Add GPS Tracker Service class

--- app/src/main/java/com/example/mobilecollection/utilities/Utilities.java
+++ app/src/main/java/com/example/mobilecollection/utilities/Utilities.java
@@ -1,8 +1,15 @@
 package com.example.mobilecollection.utilities;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -12,6 +19,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.mobilecollection.R;
+import com.example.mobilecollection.View.MainActivity;
 
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -72,4 +84,85 @@ public final class Utilities {
         return dialog;
     }
 
+    public static Location getCurrentLocation(Context ctx){
+        LocationManager lm = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
+        assert lm != null;
+        boolean isGpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkLocationEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Location networkLocation = null, gpsLocation = null, finalLocation = null;
+        if(isGpsEnabled){
+            if (ActivityCompat
+                    .checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                return null;
+            }
+            gpsLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+        if(isNetworkLocationEnabled){
+            networkLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+
+        if(gpsLocation != null && networkLocation != null){
+
+            if(gpsLocation.getAccuracy() > networkLocation.getAccuracy()){
+                return finalLocation = networkLocation;
+            } else {
+                return finalLocation = gpsLocation;
+            }
+        } else {
+            if(gpsLocation != null){
+                return finalLocation = gpsLocation;
+            } else if (networkLocation != null){
+                return finalLocation = networkLocation;
+            }
+        }
+
+        return finalLocation;
+    }
+
+    public static boolean checkLocationPermission(Activity ctx) {
+        final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+        if (ContextCompat.checkSelfPermission(ctx,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(ctx,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(ctx)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(ctx,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(ctx,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
